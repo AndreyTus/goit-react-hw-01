133b818eb27dc29a32aa4f3c199003925318bae9
AndreyTus <andrey_tus@ukr.net>
2024-03-06 21:25:34 +0200

    HW1

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,35 +1,37 @@
-// const Profile = ({ username, tag, location, image, stats: { followers, views, likes } }) => {
-const Profile = () => {
-    
-    return (
-        <div>
-  <div>
-    <img
-      src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-      alt="User avatar"
-    />
-    <p>Petra Marica</p>
-    <p>@pmarica</p>
-    <p>Salvador, Brasil</p>
-  </div>
+import React from "react";
+import css from "./Profile.module.css";
 
-  <ul>
-    <li>
-      <span>Followers</span>
-      <span>1000</span>
-    </li>
-    <li>
-      <span>Views</span>
-      <span>2000</span>
-    </li>
-    <li>
-      <span>Likes</span>
-      <span>3000</span>
-    </li>
-  </ul>
-</div>
-
-    );
+const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+}) => {
+  return (
+    <div className={css.profileContainer}>
+      <div className={css.cardDescription}>
+        <img className={css.userImage} src={avatar} alt="User avatar" />
+        <p className={css.usernameTitle}>{username}</p>
+        <p className={css.userInfo}>&#64;{tag}</p>
+        <p className={css.userInfo}>{location}</p>
+      </div>
+      <ul className={css.statsList}>
+        <li className={css.statsItem}>
+          <span className={css.listTitle}>Followers</span>
+          <span className={css.listAmount}>{followers}</span>
+        </li>
+        <li className={css.statsItem}>
+          <span className={css.listTitle}>Views</span>
+          <span className={css.listAmount}>{views}</span>
+        </li>
+        <li className={css.statsItem}>
+          <span className={css.listTitle}>Likes</span>
+          <span className={css.listAmount}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Profile;
